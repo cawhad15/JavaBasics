715af6406f77a50c644577e6da6227d9d95ecf8f
charuawhad <charushila.awhad@gmail.com>
2020-06-24 16:36:54 -0700

    Swap Program

---
+++ JAVA Basics/src/test/java/test/art1/TwoNumberSwap.java
@@ -0,0 +1,29 @@
+package test.art1;
+
+public class TwoNumberSwap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=13;
+		int y=14;
+		
+	     int k = 0;
+	     
+	    // k=x+y;
+	     System.out.println("Before Swapping \t" +x+y);
+	   
+	     
+	   // for(int i=0;i<3;i++)
+	    //{
+	       k=x;
+	       x=y;
+	       y=k;
+	    //}
+	     
+	    System.out.println("After Swapping \t" +x+y);
+	    // System.out.println(x);
+	    // System.out.println(y);
+
+	}
+
+}
