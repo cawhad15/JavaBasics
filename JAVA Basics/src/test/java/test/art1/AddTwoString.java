d6e2ca7de8817ad41c12c1acddffec4d1d242929
charuawhad <charushila.awhad@gmail.com>
2020-06-24 16:39:05 -0700

    Add two string

---
+++ JAVA Basics/src/test/java/test/art1/AddTwoString.java
@@ -0,0 +1,17 @@
+package test.art1;
+
+public class AddTwoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   String str1= "This test is for mobileforming";
+	        
+	        //Check if the string contains "mobileforming" substring
+	        
+	        System.out.println("Contains" +str1.contains("forming"));
+	
+
+	}
+
+}
